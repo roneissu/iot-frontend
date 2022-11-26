e82c1656f3be8fa328c470cc1d1d58d1700ac02e
roneissu <sadeluisronaldo@gmail.com>
2022-11-26 19:16:29 -0300

    Add info in home page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
+    MatButtonModule,
     HomeRoutingModule
   ]
 })
